b5d7182be606c9195c05a5dccabd2776e6e9be3a
akg007 <aniruddhagawali03@gmail.com>
2023-11-18 13:16:41 +0530

    Add new files and dependencies

---
+++ client/src/app/register/farmer/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function FarmerLogin() {
+  return (
+    <div>
+      <section></section>
+      <section>
+        <div>
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            Register
+          </h3>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            Register as Farmer
+          </p>
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
+
+export default FarmerLogin;
