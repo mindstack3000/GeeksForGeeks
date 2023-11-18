626dcd50024a0e40fb1bdc6eedb0412af392d4b6
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-11-19 04:14:24 +0530

    Refactor header component styles

--- client/src/components/homePage/header.jsx
+++ client/src/components/homePage/header.jsx
@@ -1,16 +1,46 @@
+// import React from "react";
+// import { Button } from "../ui/button";
+// import Link from "next/link";
+
+// function Header() {
+//   return (
+//     <div className="flex h-24 w-full items-center justify-between bg-primary ">
+//       <div className="mx-3">
+//         <Button>
+//           <h1 className="text-3xl font-bold text-white">Fresh Flow</h1>
+//         </Button>
+//       </div>
+//       <div className="mx-5 flex justify-evenly">
+//         <span className="mx-3">
+//           <Button asChild className="bg-white text-primary hover:bg-secondary">
+//             <Link href="/register/farmer">Register</Link>
+//           </Button>
+//         </span>
+//         <span className="mx-3">
+//           <Button asChild className="bg-white text-primary hover:bg-secondary">
+//             <Link href="/login">Login</Link>
+//           </Button>
+//         </span>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 
 function Header() {
   return (
-    <div className="flex h-24 w-full items-center justify-between bg-primary ">
-      <div className="mx-3">
+    <div className="flex flex-col sm:flex-row sm:h-24 w-full items-center justify-between bg-primary">
+      <div className="mx-3 mb-3 sm:mb-0">
         <Button>
           <h1 className="text-3xl font-bold text-white">Fresh Flow</h1>
         </Button>
       </div>
-      <div className="mx-5 flex justify-evenly">
+      <div className="mx-5 mb-2 sm:mb-0 flex justify-evenly">
         <span className="mx-3">
           <Button asChild className="bg-white text-primary hover:bg-secondary">
             <Link href="/register/farmer">Register</Link>
