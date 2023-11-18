9760e05e4fc19bb0b374f2e2f7a6e5029650c8a8
akg007 <aniruddhagawali03@gmail.com>
2023-11-18 20:37:31 +0530

    Refactor input_with_label component and add form handling logic in WareHouseRegister

--- client/src/app/register/warehouse/page.jsx
+++ client/src/app/register/warehouse/page.jsx
@@ -7,13 +7,51 @@ import RegisterSelector from "@/components/register_selector";
 
 const typesOfStorage = ["Hot Storage", "Cold Storage"];
 
+const cropTypes = [
+  "Wheat",
+  "Rice",
+  "Corn",
+  "Barley",
+  "Soybeans",
+  "Cotton",
+  "Potatoes",
+  "Tomatoes",
+  "Fruits",
+  "Vegetables",
+  "Other",
+];
+
 function WareHouseRegister() {
   const [selectTypeOfStorage, setSelectTypeOfStorage] = useState("");
-  // const [form , setForm] = useState({
-  //   "name" : "",
-  //   "username" : "",
-  //   "location" : "",
+  const [selectCropTypes, setSelectCropTypes] = useState([]);
 
+  const [form, setForm] = useState({
+    name: "",
+    username: "",
+    password: "",
+    location: "",
+    temp_low: "",
+    temp_high: "",
+    capacity: "",
+    tempType: "",
+    certifications: "",
+    security: "",
+    phoneNo: "",
+    email: "",
+    servicesOffered: "",
+    price: "",
+    typeOfCrop: [],
+  });
+
+  const handleChange = (e, name) => {
+    const { value } = e.target;
+    setForm({ ...form, [name]: value });
+    console.log(form);
+  };
+
+  const handleSubmit = async (e) => {
+    console.log("form", form);
+  };
 
   return (
     <>
@@ -29,19 +67,43 @@ function WareHouseRegister() {
         </div>
 
         <div className="mt-10 flex w-full flex-col items-center justify-center gap-5">
-          <InputWithLabel label="Warehouse" />
-          <InputWithLabel label="Fullname" />
-          <InputWithLabel label="Username" />
-          <InputWithLabel label="Password" type="password" />
-          <InputWithLabel label="Phone No." type="text" />
-          <InputWithLabel label="Email" type="text" />
-          <InputWithLabel label="Address" type="text" />
-          <InputWithLabel label="Capacity" type="text" />
-          <InputWithLabel label="Temperature Control" type="text" />
+          <InputWithLabel
+            label="Fullname"
+            onChange={(e) => handleChange(e, "name")}
+          />
+          <InputWithLabel
+            label="Username"
+            onChange={(e) => handleChange(e, "username")}
+          />
+          <InputWithLabel
+            label="Password"
+            type="password"
+            onChange={(e) => handleChange(e, "password")}
+          />
+          <InputWithLabel
+            label="Phone No."
+            type="text"
+            onChange={(e) => handleChange(e, "phoneNo")}
+          />
+          <InputWithLabel
+            label="Email"
+            type="text"
+            onChange={(e) => handleChange(e, "email")}
+          />
+          <InputWithLabel
+            label="Address"
+            type="text"
+            onChange={(e) => handleChange(e, "address")}
+          />
+          <InputWithLabel
+            label="Capacity"
+            type="text"
+            onChange={(e) => handleChange(e, "capacity")}
+          />
 
           <div className="flex w-full flex-col items-start justify-start gap-5 sm:flex-row">
             <label className="w-1/5  whitespace-nowrap text-lg font-normal tracking-tight">
-              Types of Crop
+              Types of Temp
             </label>
             <div className="flex w-4/5 flex-wrap items-start justify-start gap-3">
               {typesOfStorage.map((cropType) => (
@@ -64,10 +126,58 @@ function WareHouseRegister() {
               ))}
             </div>
           </div>
+
+          <div className="flex w-full flex-col items-start justify-start gap-5 sm:flex-row">
+            <label className="w-1/5  whitespace-nowrap text-lg font-normal tracking-tight">
+              Temperature
+            </label>
+
+            <div className="flex w-4/5 flex-wrap items-start justify-start gap-3">
+              <InputWithLabel
+                label="Low"
+                type="text"
+                onChange={(e) => handleChange(e, "temp_low")}
+              />
+              <InputWithLabel
+                label="High"
+                type="text"
+                onChange={(e) => handleChange(e, "temp_high")}
+              />
+            </div>
+          </div>
+
+          <div className="flex w-full flex-col items-start justify-start gap-5 sm:flex-row">
+            <label className="w-1/5  whitespace-nowrap text-lg font-normal tracking-tight">
+              Types of Crop
+            </label>
+            <div className="flex w-4/5 flex-wrap items-start justify-start gap-3">
+              {cropTypes.map((cropType) => (
+                <Button
+                  variant={
+                    selectCropTypes.includes(cropType) ? "secondary" : "outline"
+                  }
+                  onClick={() => {
+                    if (selectCropTypes.includes(cropType)) {
+                      setSelectCropTypes(
+                        selectCropTypes.filter((crop) => crop !== cropType),
+                      );
+                    } else {
+                      setSelectCropTypes([...selectCropTypes, cropType]);
+                    }
+                  }}
+                >
+                  {cropType}
+                </Button>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
+
       <div className="mt-8 flex w-full items-center justify-center gap-5">
-        <Button className=" w-2/3">Register</Button>
+        <Button className=" w-1/3" onClick={handleSubmit}>
+          Register
+        </Button>
       </div>
     </>
   );
