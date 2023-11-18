b19ed8e04cc0934aa6512b667a48c2730c097aac
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-11-18 16:41:38 +0530

    refactor:links updated

--- client/src/components/ui/footer.jsx
+++ client/src/components/ui/footer.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className='w-full h-auto bg-black text-white p-4 md:p-8 relative bottom-0'>
-      <div className="flex flex-col items-center md:flex-row justify-between">
-        <div className='flex flex-col items-center md:items-start text-center md:text-left mb-4 md:mb-0'>
-          <ul className="flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-4 md:list-disc md:list-inside">
-            <li><a href='/pages/terms'>Terms of Use</a></li>
-            <li><a href='/pages/registerPage'>Register</a></li>
-            <li><a href='/pages/logInPage'>LogIn</a></li>
+    <div className="relative bottom-0 h-auto w-full bg-black p-4 text-white md:p-8">
+      <div className="flex flex-col items-center justify-between md:flex-row">
+        <div className="mb-4 flex flex-col items-center text-center md:mb-0 md:items-start md:text-left">
+          <ul className="flex flex-col space-y-2 md:list-inside md:list-disc md:flex-row md:space-x-4 md:space-y-0">
+            <li>
+              <Link href="/">Terms of Use</Link>
+            </li>
+            <li>
+              {" "}
+              <Link href="/register/farmer">Register</Link>
+            </li>
+            <li>
+              <Link href="/login">Login</Link>
+            </li>
           </ul>
-          <p className="mt-2 md:mt-0">Copyright @2023 MindStack Inc. All rights reserved.</p>
+          <p className="mt-2 md:mt-0">
+            Copyright @2023 MindStack Inc. All rights reserved.
+          </p>
         </div>
-        <div className='flex justify-center items-center md:ml-8'>
+        <div className="flex items-center justify-center md:ml-8">
           <h3 className="text-sm md:text-xl">Mindstack</h3>
-          <Image src={'/Mindstack.png'} width={40} height={40} className="ml-2 rounded-3xl" />
+          <Image
+            src={"/Mindstack.png"}
+            width={40}
+            height={40}
+            className="ml-2 rounded-3xl"
+          />
         </div>
       </div>
     </div>
-  )
+  );
 };
