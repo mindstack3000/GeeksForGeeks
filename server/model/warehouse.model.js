d62349dbf7885191627a3753e734fe7c70862159
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-11-18 12:27:58 +0530

    warehousemodel

---
+++ server/model/warehouse.model.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const client = require('../config/connect');
+
+const facilites = {
+    temperature:{
+        low:{
+            type: Number,
+            required: true,
+        },
+        high:{
+            type: Number,
+            required: true,
+        }
+    },
+    capacity:{
+        type: Number,
+        required: true,
+    },
+    tempType:{
+        type: String,
+        required: true,
+    }
+}
+
+const hours = {
+    open:{
+        type: Number,
+        required: true,
+    },
+    close:{
+        type: Number,
+        required: true,
+    }
+
+}
+
+const warehouseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    location:{
+        type: String,
+        required: true,
+    },
+    facility:{
+        type: String,
+        enum: facilites,
+        required: true,
+    },
+    certifications:{
+        type: String,
+        required: true,
+    },
+    security:{
+        type: [String],
+        required: true,
+    },
+    phone:{
+        type: Number,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    otp:{
+        type: Number,
+        required: false,    
+    },
+    oeratingHours:{
+        type: String,
+        enum: hours,
+        required: true,
+    },
+})
+
+const Warehouse = client.model('Warehouse', warehouseSchema);
+module.exports = Warehouse;
