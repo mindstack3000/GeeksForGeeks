93616e11fe3576d1de921758b5bc23c9aac292a6
Kalpesh_Nimje <kalpesh22nimje@gmail.com>
2023-11-19 02:38:07 +0530

    terms and conditions added

---
+++ client/src/components/terms&conditions.jsx
@@ -0,0 +1,118 @@
+export default function TermsAndConditions() {
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+      <p>
+        <b>FreshFlow - Terms and Conditions</b>
+        Please read the following terms and conditions carefully before using
+        the FreshFlow. By accessing or using our platform, you agree to be bound
+        by these terms and conditions.
+        <ol>
+          <li>
+            <b>Acceptance of Terms:</b>
+            <div>
+              - By using the FreshFlow platform, you agree to comply with and be
+              bound by these terms and conditions. If you do not agree to these
+              terms, please do not use our website.
+            </div>
+          </li>
+          <li>
+            <b>User Accounts:</b>
+            <div>
+              - Users must create an account to access certain features of the
+              platform. - You are responsible for maintaining the
+              confidentiality of your account information. - You agree to notify
+              us immediately of any unauthorized use of your account.
+            </div>
+          </li>
+          <li>
+            <b>Role-based Usage:</b>
+            <div>
+              - Users must select a role as a warehouse owner, farmer, or
+              customer. - Each role has specific functionalities and
+              responsibilities associated with it.
+            </div>
+          </li>
+          <li>
+            <b>Content Guidelines:</b>
+            <div>
+              - Users are responsible for the accuracy and legality of the
+              content they post on the platform. - Prohibited content includes
+              but is not limited to illegal, offensive, or misleading
+              information.
+            </div>
+          </li>
+          <li>
+            <b>Transactions:</b>
+            <div>
+              - Transactions between warehouse owners, farmers, and customers
+              are the sole responsibility of the involved parties and
+              transaction portals used. - FreshFlow is not liable for any
+              disputes arising from transactions on the platform.
+            </div>
+          </li>
+          <li>
+            <b>Privacy Policy:</b>
+            <div>
+              - Our Privacy Policy outlines how we collect, use, and protect
+              your personal information. - By using our platform, you consent to
+              the practices outlined in the Privacy Policy.
+            </div>
+          </li>
+          <li>
+            <b>Intellectual Property:</b>
+            <div>
+              - The platform's content, including but not limited to text,
+              graphics, logos, and images, is the property of FreshFlow. - Users
+              may not reproduce, distribute, or modify any content without
+              written permission.
+            </div>
+          </li>
+          <li>
+            <b>Security:</b>
+            <div>
+              - Users are prohibited from violating the security of the
+              platform. - Any attempt to breach security, tamper with data, or
+              interfere with the operation of the website is strictly
+              prohibited.
+            </div>
+          </li>
+          <li>
+            <b>Termination of Access:</b>
+            <div>
+              - FreshFlow reserves the right to terminate or suspend user
+              accounts without prior notice for violation of these terms and
+              conditions.
+            </div>
+          </li>
+          <li>
+            <b>Modifications to Terms:</b>
+            <div>
+              - FreshFlow may modify these terms and conditions at any time. -
+              Users will be notified of changes, and continued use of the
+              platform constitutes acceptance of the modified terms.
+            </div>
+          </li>
+          <li>
+            <b>Limitation of Liability:</b>
+            <div>
+              - FreshFlow is not liable for any direct, indirect, incidental,
+              consequential, or punitive damages arising from the use of the
+              platform.
+            </div>
+          </li>
+          <li>
+            <b>Contact Information:</b>
+            <div>
+              - For questions or concerns regarding these terms, please contact
+              us at
+              <a href="mailto:aniruddhagawali05@gmail.com" target="_blank"></a>.
+            </div>
+          </li>
+        </ol>
+        By using the FreshFlow, you acknowledge that you have read, understood,
+        and agree to be bound by these terms and conditions.
+      </p>
+    </div>
+  );
+}
