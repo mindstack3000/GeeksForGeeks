edbe4029bd98a8eda87cefc54a6f8573a9b77a43
akg007 <aniruddhagawali03@gmail.com>
2023-11-19 04:42:58 +0530

    Refactor popUp component

--- client/src/components/popUp.jsx
+++ client/src/components/popUp.jsx
@@ -1,39 +1,41 @@
 "use client";
-import React from 'react';
-import InputWithLabel from './input_with_label';
-import { useState } from 'react';
-import { Button } from './ui/button';
+import React from "react";
+import InputWithLabel from "./input_with_label";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 const PopUp = ({ onClose }) => {
-    const [isOpened, setIsOpened] = useState(true);
-    const toggle = () => setIsOpened(!isOpened);
-    React.useEffect(() => {
-      if (isOpened) {
-        document.body.style.overflow = "hidden";
-      } else {
-        document.body.style.overflow = "unset";
-      }
-    }, [isOpened]);
-    if (!isOpened) return null;
+  const [isOpened, setIsOpened] = useState(true);
+  const toggle = () => setIsOpened(!isOpened);
+  React.useEffect(() => {
+    if (isOpened) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isOpened]);
+  if (!isOpened) return null;
   return (
-    <div className='flex flex-col w-80 border border-input p-2  z-30'>
-      <button className="self-end p-2" 
-        onClick={() => {
+    <div className="fixed left-0 top-0 z-20 h-full w-full bg-black bg-opacity-50">
+      <div className="sticky left-1/2 top-1/2 z-30 flex h-1/3 w-1/3 -translate-x-1/2 -translate-y-1/2 flex-col rounded-md border border-input bg-white p-2">
+        <button
+          className="self-end p-2"
+          onClick={() => {
             toggle();
             onClose();
-        }}
-
-      >
-        Close
-      </button>
-      <span className='m-4'>
-        <InputWithLabel label={"Quantity"}/>
-      </span>
-      <span className='m-4'>
-        <InputWithLabel label={"Duration"}/>
-      </span>
-      <div className='flex justify-end m-1'>
-        <Button>Send Request</Button>
+          }}
+        >
+          Close
+        </button>
+        <span className="m-4">
+          <InputWithLabel label={"Quantity"} type="number" />
+        </span>
+        <span className="m-4">
+          <InputWithLabel label={"Duration"} type="number" />
+        </span>
+        <div className="m-1 flex justify-end">
+          <Button>Send Request</Button>
+        </div>
       </div>
     </div>
   );
