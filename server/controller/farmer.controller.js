774b592ade60a3c9951134c5ba43fb5e348a5158
Freakyab <aryanbhisikar1@gmail.com>
2023-11-18 12:34:23 +0530

    Add update and delete routes for farmer controller

--- server/controller/farmer.controller.js
+++ server/controller/farmer.controller.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const Account = require("../models/account.model");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth.middleware");
 
 /*
  * @route POST /farmer/register
@@ -106,9 +107,82 @@ router.post("/login", async (req, res) => {
   }
 });
 
-
 /*
  * @route PUT /farmer/update
  */
 
-// router.put("/update", async (req, res) => {
+router.put("/update/:id", auth, async (req, res) => {
+  try {
+    const {
+      adharNo,
+      fullName,
+      phoneNo,
+      email,
+      address,
+      username,
+      password,
+      landSize,
+      typeOfCrop,
+    } = req.body;
+
+    // Check if the user already exists
+    const user = await Account.findOne({ _id: req.params.id });
+    if (!user) return res.status(400).json({ msg: "User does not exists" });
+
+    // Create a new user
+    const newUser = new Account({
+      adharNo,
+      fullName,
+      phoneNo,
+      email,
+      address,
+      username,
+      password,
+      landSize,
+      typeOfCrop,
+    });
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Replace the password with the hashed password
+    newUser.password = hashedPassword;
+
+    // Save the user
+    await Account.updateOne({ _id: req.params.id }, newUser);
+
+    // Sign the token
+    const token = jwt.sign(
+      { id: newUser._id, username: newUser.username },
+      process.env.JWT_SECRET
+    );
+
+    // Send the token in a HTTP-only cookie
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+/*
+ * @route Delete /farmer/delete/:id
+ */
+
+router.delete("/delete/:id", auth, async (req, res) => {
+  try {
+    const user = await Account.findOne({ _id: req.params.id });
+    if (!user) return res.status(400).json({ msg: "User does not exists" });
+
+    await Account.findByIdAndDelete(req.params.id);
+    res.json({ msg: "User deleted" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
