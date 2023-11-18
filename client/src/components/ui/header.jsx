4dcb8087d4817128b456f4239e16c7046382eab4
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2023-11-18 16:48:11 +0530

    refactor:header links updated

--- client/src/components/ui/header.jsx
+++ client/src/components/ui/header.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { Button } from "./button";
-
+import Link from "next/link";
 
 function Header() {
   return (
-    <div className="flex w-full h-24 items-center justify-between bg-primary ">
-        <div>
+    <div className="flex h-24 w-full items-center justify-between bg-primary ">
+      <div>
         <Button>FreshFlow</Button>
-        </div>
-        <div className="m-5 flex justify-evenly">
-        <Button>Register</Button>
-        <Button>LogIn</Button>
-        </div>
+      </div>
+      <div className="mx-5 flex justify-evenly">
+        <span className="mx-3">
+
+        <Button asChild className="bg-white text-primary hover:bg-secondary">
+          <Link href="/register/farmer">Register</Link>
+        </Button>
+        </span>
+        <span className="mx-3">
+
+        <Button asChild className="bg-white text-primary hover:bg-secondary">
+          <Link href="/login">Login</Link>
+        </Button>
+        </span>
+      </div>
     </div>
   );
 }
