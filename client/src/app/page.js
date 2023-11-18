8e0d15eb261a067d144a4f7f1eb814f1ff67c17c
akg007 <aniruddhagawali03@gmail.com>
2023-11-18 11:41:46 +0530

    first commit

---
+++ client/src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-red-500">hello world</h1>
+      <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+    </main>
+  );
+}
